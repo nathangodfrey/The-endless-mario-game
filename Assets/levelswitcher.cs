343fc5e99bb59c590022052251dfde30a196b74c
phill765 <league@WTS13.local>
2016-08-13 12:01:48 -0700

    endless mario game

---
+++ Assets/levelswitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelswitcher : MonoBehaviour {
+
+	public string nextLevel ;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			SceneManager.LoadScene (nextLevel);
+		}
+	}
+}
---
+++ Assets/scripts/endlevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class endlevel : MonoBehaviour {
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Player") {
+			SceneManager.LoadScene (level);
+		}
+	}
+	public string level;
+}
